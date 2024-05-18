3b7b72fe84b54d81e4329f71f1d1809f74f80070
pbarrios <pbarrios@magnasistemas.com.br>
2024-05-17 21:38:14 -0300

    Os Verbos HTTP POST, PUT e DELETE no Spring Framework e Spring Boot

--- src/main/java/br/com/erudio/demo/controllers/PersonController.java
+++ src/main/java/br/com/erudio/demo/controllers/PersonController.java
@@ -4,10 +4,7 @@ import br.com.erudio.demo.model.Person;
 import br.com.erudio.demo.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,24 @@ public class PersonController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id) {
         return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") String id) {
+        service.delete(id);
     }
 }
