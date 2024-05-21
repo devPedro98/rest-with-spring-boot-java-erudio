5521985f99ed8694a565f10a03586d3cc37af546
pbarrios <pbarrios@magnasistemas.com.br>
2024-05-21 16:23:48 -0300

    Removendo Warnings após o Upgrade p. Spring Boot 3.2.0 e Java 21

--- src/main/java/br/com/erudio/demo/repositories/PersonRepository.java
+++ src/main/java/br/com/erudio/demo/repositories/PersonRepository.java
@@ -2,8 +2,7 @@ package br.com.erudio.demo.repositories;
 
 import br.com.erudio.demo.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
