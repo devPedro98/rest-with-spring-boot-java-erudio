a62ef0aceea7223681c205d26ad515bf78d9d98f
pbarrios <pbarrios@magnasistemas.com.br>
2024-05-23 22:47:47 -0300

    Content Negotiation via HEADER Parameter

--- src/main/java/br/com/erudio/demo/config/WebConfig.java
+++ src/main/java/br/com/erudio/demo/config/WebConfig.java
@@ -9,9 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // via query param
+        //http://localhost:8080/api/person/v1/1?mediaType=xml
+        /*
         configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+        */
+
+        // via header param http://localhost:8080/api/person/v1/1
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
