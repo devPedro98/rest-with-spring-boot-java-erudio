1a640da0a20d1b4266dd82429ac92305ddb26bcc
pbarrios <pbarrios@magnasistemas.com.br>
2024-06-11 15:20:48 -0300

    teste para validar se o swagger esta funcionando

--- src/test/java/br/com/erudio/demo/integrationtests/testcontainers/AbstractIntegrationTest.java
+++ src/test/java/br/com/erudio/demo/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -1,5 +1,8 @@
 package br.com.erudio.demo.integrationtests.testcontainers;
 
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -8,35 +11,34 @@ import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.lifecycle.Startables;
 
-import java.util.Map;
-import java.util.stream.Stream;
-
 @ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
 public class AbstractIntegrationTest {
 
-    public class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
         static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.29");
 
-       private static void startContainers(){
-           Startables.deepStart(Stream.of(mysql)).join();
-       }
+        private static void startContainers() {
+            Startables.deepStart(Stream.of(mysql)).join();
+        }
 
-        private static Map<String, String> createCoonectionConfiguration() {
+        private static Map<String, String> createConnectionConfiguration() {
             return Map.of(
                     "spring.datasource.url", mysql.getJdbcUrl(),
-                    "spring.datasource.username", mysql.getJdbcUrl(),
-                    "spring.datasource.password", mysql.getJdbcUrl()
+                    "spring.datasource.username", mysql.getUsername(),
+                    "spring.datasource.password", mysql.getPassword()
             );
         }
+
         @SuppressWarnings({"unchecked", "rawtypes"})
         @Override
         public void initialize(ConfigurableApplicationContext applicationContext) {
             startContainers();
             ConfigurableEnvironment environment = applicationContext.getEnvironment();
-            MapPropertySource testcontainers = new MapPropertySource("testcontainers",
-                    (Map) createCoonectionConfiguration());
+            MapPropertySource testcontainers = new MapPropertySource(
+                    "testcontainers",
+                    (Map) createConnectionConfiguration());
             environment.getPropertySources().addFirst(testcontainers);
         }
-
     }
 }
