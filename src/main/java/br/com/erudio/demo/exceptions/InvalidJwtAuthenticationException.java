d9349b317da1e2040faccd2844666ecf4a4899fb
pbarrios <pbarrios@almavivasolutions.com.br>
2024-06-21 10:36:11 -0300

    Criando Exceptions Customizadas e o VO de Credenciais

---
+++ src/main/java/br/com/erudio/demo/exceptions/InvalidJwtAuthenticationException.java
@@ -0,0 +1,14 @@
+package br.com.erudio.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.security.core.AuthenticationException;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class InvalidJwtAuthenticationException extends AuthenticationException {
+    private static final long serialVersionUID = 1L;
+
+    public InvalidJwtAuthenticationException(String ex) {
+        super(ex);
+    }
+}
--- src/main/java/br/com/erudio/demo/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/br/com/erudio/demo/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.erudio.demo.exceptions.handler;
 
 import br.com.erudio.demo.exceptions.ExceptionResponse;
+import br.com.erudio.demo.exceptions.InvalidJwtAuthenticationException;
 import br.com.erudio.demo.exceptions.RequiredObjectIsNullException;
 import br.com.erudio.demo.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -41,5 +42,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(InvalidJwtAuthenticationException.class)
+    public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
     }
 }
