fe412ecda198753031b9be29dda6d9d25688d18d
pbarrios <pbarrios@magnasistemas.com.br>
2024-07-18 12:37:46 -0300

    Retornando um PagedModel

--- src/main/java/br/com/erudio/demo/services/PersonServices.java
+++ src/main/java/br/com/erudio/demo/services/PersonServices.java
@@ -11,8 +11,11 @@ import br.com.erudio.demo.model.Person;
 import br.com.erudio.demo.repositories.PersonRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
@@ -30,12 +33,17 @@ public class PersonServices {
     @Autowired
     PersonMapper mapper;
 
-    public Page<PersonVO> findAll(Pageable pageable) {
+    @Autowired
+    PagedResourcesAssembler<PersonVO> assembler;
+
+    public PagedModel<EntityModel<PersonVO>> findAll(Pageable pageable) {
         logger.info("Findind all people");
         var personPage = repository.findAll(pageable);
         var personVosPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
         personVosPage.map(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
-        return personVosPage;
+        Link link = linkTo(methodOn(PersonController.class)
+                .findAll(pageable.getPageNumber(), pageable.getPageSize(), "asc")).withSelfRel();
+        return assembler.toModel(personVosPage, link);
     }
 
     public PersonVO findById(Long id) {
